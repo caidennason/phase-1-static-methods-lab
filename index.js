f1502a52a32bac12a1ff5d990dfa52e52d936ca6
caiden nason <caidenbnason@gmail.com>
2022-06-23 17:20:37 -0400

    commit tree 2604bfdee3cb6cebe261c8c17a289a241d683cd8

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    let fixedWord = string[0].toUpperCase() + string.slice(1)
+    return fixedWord 
+  }
+  static sanitize(string){
+  const replaced = string.replace(/[^-, 'A-Za-z0-9]+/g, '')
+  return replaced
+  }
+  static titleize(string){
+    let wordArr = []
+    let words = ['the',  'a',  'an',
+    'but',  'of', 'and',
+    'for',  'at', 'by',
+    'from']
+    let parts = string.split(' ')
+    for (let i = 0; i < parts.length; i++){
+    if (i == 0){
+      wordArr.push(this.capitalize(parts[i]))
+      console.log(wordArr)
+    } else {
+      if (words.includes(parts[i])){
+        wordArr.push(parts[i])
+      } else {
+        wordArr.push(this.capitalize(parts[i]))
+      }
+    }
+  }
+  return wordArr.join(' ')
+}
 }
